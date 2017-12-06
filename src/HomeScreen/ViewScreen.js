1aad5e4d38400c3cfa44fcb1b051ea196657a455
Shivan Amin <shivanamin@Shivans-MacBook-Pro.local>
2017-12-06 08:39:25 -0600

    highlight

--- src/HomeScreen/ViewScreen.js
+++ src/HomeScreen/ViewScreen.js
@@ -3,6 +3,8 @@ import { Text, Container, Card, CardItem, Body, Content, Header, Left, Right, Ic
 import {Main_styles as styles} from './../../Styles/App_styles';
 import { StyleSheet,View, Alert } from 'react-native';
 import {Actions} from "react-native-router-flux";
+import SyntaxHighlighter from 'react-native-syntax-highlighter';
+import { ocean } from 'react-syntax-highlighter/dist/styles';
 
 
 export default class ViewScreen extends React.Component {
@@ -28,17 +30,21 @@ export default class ViewScreen extends React.Component {
       </Header>
     )
   });
-  render() {
 
+
+  render() {
     return (
       <Container>
-
         <Content padder>
+
+
           <Text>Title: {this.props.data.title}</Text>
+          <Text>---</Text>
+          <Text>Code:</Text>
           <Card>
-            <CardItem>
-              <Text>{this.props.data.codeEntry}</Text>
-            </CardItem>
+
+              <SyntaxHighlighter language={this.props.data.language} style={ocean}>{this.props.data.codeEntry}</SyntaxHighlighter>
+            
           </Card>
           <Text>Comments:</Text>
           <Card>
